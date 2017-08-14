3abd13efbbfba9aef64f9893f54dc58e7e5d3342
Tim Humphries <thumphries5@hotmail.com>
2017-08-14 15:46:43 +0100

    android environment update

--- src/reducers/appScreens/homeReducer.js
+++ src/reducers/appScreens/homeReducer.js
@@ -2,5 +2,11 @@ import * as types from '../../constants/actionTypes';
 import initialState from '../initialState';
 
 export default function (state = initialState, action){
-    
+    switch(action.type){
+        case types.RETRIEVE_COURSE_DETAILS:
+        return{
+            ...state
+        };
+        default: return state;
+    }
 }
