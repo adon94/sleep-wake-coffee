e5e38ca70204e7fa2839b19c53a9eb9e12a7e129
Tim Humphries <thumphries5@hotmail.com>
2017-08-14 14:41:38 +0100

    folder structure

---
+++ src/reducers/appScreens/homeReducer.js
@@ -0,0 +1,6 @@
+import * as types from '../../constants/actionTypes';
+import initialState from '../initialState';
+
+export default function (state = initialState, action){
+    
+}
---
+++ src/reducers/initialState.js
@@ -0,0 +1,7 @@
+export default {
+    projectData: {
+        details: {
+            "name": "My name"
+        }
+    }
+}
