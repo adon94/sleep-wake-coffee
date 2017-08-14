f35fe576d8a569ced4e6ef827fd7333c1e9ae765
Tim Humphries <thumphries5@hotmail.com>
2017-08-14 16:27:25 +0100

    working first half

--- src/screens.js
+++ src/screens.js
@@ -1,1 +1,7 @@
-import { Navigation } from 'react-redux-navigation'
+import { Navigation } from 'react-native-navigation';
+
+import Home from './modules/appScreens/Home';
+
+export function registerScreens(store, Provider) {
+    Navigation.registerComponent('app.Home', () => Home, store, Provider);
+}
